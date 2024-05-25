1dbfd343c833e9425abf0bdaa699153f1714c05d
AshGw <AshrefGw@proton.me>
2024-05-25 17:28:48 +0100

    feat: impement debug for errors

--- src/error.rs
+++ src/error.rs
@@ -1,21 +1,16 @@
 use std::fmt::{self, Display, Formatter};
 
+
 #[derive(Debug, PartialEq)]
 pub enum Error {
-    UnknownAlgorithm(&'static str, String),
     InvalidKeySize,
     InvalidNonceSize,
-    InvalidInputSize,
     InvalidTag,
 }
 
 impl Display for Error {
     fn fmt(&self, fmt: &mut Formatter) -> fmt::Result {
         match self {
-            Error::UnknownAlgorithm(algo, name) => {
-                write!(fmt, "unknown {} algorithm {}", algo, name)
-            }
-            Error::InvalidKeySize => write!(fmt, "invalid key size"),
             Error::InvalidNonceSize => {
                 write!(fmt, "invalid nonce size")
             }
