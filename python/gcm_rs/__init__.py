375247a6be77ece67b4bbc987abe4a331b05760b
AshGw <AshrefGw@proton.me>
2024-05-31 06:49:00 +0100

    feat: bind `GCM` intial bindings

--- python/gcm_rs/__init__.py
+++ python/gcm_rs/__init__.py
@@ -1,3 +1,4 @@
 from gcm_rs import *
 
 print(gen_key())
+a = GCM()
--- rust/lib.rs
+++ rust/lib.rs
@@ -5,6 +5,7 @@ pub mod gcm;
 pub mod random;
 pub mod types;
 
+use gcm::Aes256Gcm;
 use random::{gen_key as gk, gen_nonce as gn};
 
 use pyo3::prelude::*;
@@ -19,10 +20,14 @@ fn gen_key() -> PyResult<Vec<u8>> {
     Ok(gk())
 }
 
+#[pyclass]
+struct Gcm(Aes256Gcm);
+
 #[pymodule]
 #[pyo3(name = "_lib_name")]
 fn pyrust(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(gen_nonce, m)?)?;
     m.add_function(wrap_pyfunction!(gen_key, m)?)?;
+    m.add_class::<Gcm>()?;
     Ok(())
 }
